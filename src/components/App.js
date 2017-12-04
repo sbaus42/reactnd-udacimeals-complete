85b8b44242d5c7b1ff77ee366a35411757b05f89
Santiago Baus <baus.dev@gmail.com>
2017-12-04 17:46:31 -0500

    used the connect api from redux

--- src/components/App.js
+++ src/components/App.js
@@ -1,45 +1,42 @@
 import React, { Component } from 'react'
-import { addRecipe } from '../actions'
+import { connect } from 'react-redux'
+import { addRecipe, removeFromCalendar } from '../actions'
 
 class App extends Component {
-  state = {
-    calendar: null
-  }
-
-  componentDidMount() {
-    const { store } = this.props
-    store.subscribe(() => {
-      this.setState(() => ({
-        calendar: store.getState()
-      }))
-    })
-  }
-
-  submitFood = () => {
-    this.props.store.dispatch(addRecipe({
-      day: 'monday',
-      meal: 'breakfast',
-      recipe: {
-        label: this.input.value
-      }
-    }))
-  }
-
   render() {
     return (
       <div>
-        <input
-          type='text'
-          ref={(input) => this.input = input}
-          placeholder="Monday's Breakfast"
-        />
-        <button onClick={this.submitFood}>Submit</button>
-        <pre>
-          Monday's Breakfast: {this.state.calendar && this.state.calendar.monday.breakfast}
-        </pre>
+        Hello World
       </div>
     )
   }
 }
 
-export default App
+function mapDispatchToProps(dispatch) {
+  return {
+    addRecipe: (data) => dispatch(addRecipe(data)),
+    removeFromCalendar: (data) => dispatch(removeFromCalendar(data))
+  }
+}
+
+// Use this function to change how the date is displayed formatted
+// as the function suggests, this function converts the state of the
+// app to props usable by the component... I believe this also
+// connects the component to the store's state, meaning the component
+// will update whenever the state changes
+function mapStateToProps(calendar) {
+  const dayOrder = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
+
+  return {
+    calendar: dayOrder.map((day) => ({
+      day,
+      meals: Object.keys(calendar[day]).reduce((meals, meal) => {
+        meals[meal] = calendar[day][meal]
+          ? calendar[day][meal] : null
+        return meals
+      }, {})
+    }))
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
